3618e349802418c7ee8317da0134f08e751f133d
Beatrice <beatrice.opara@softwire.com>
2020-02-10 15:31:02 +0000

    printed next 5 buses

--- BusBoard.ConsoleApp/Program.cs
+++ BusBoard.ConsoleApp/Program.cs
@@ -1,6 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Security.Cryptography.X509Certificates;
+using System.Threading;
+using Microsoft.VisualBasic;
 using RestSharp;
 using RestSharp.Authenticators;
 
@@ -16,7 +19,22 @@ namespace BusBoard
         }
         private static void DisplayBusStops(List<BusStopObjects> response)
         {
-            Console.WriteLine(response[0].stationName);
+            var allBuses = response;
+            var firstFiveItems = response.Take(5);
+
+            foreach (var bus in firstFiveItems)
+            {
+                Console.WriteLine(bus.destinationName);
+            }
+            
+            // List<BusStopObjects> myBusList = new List<BusStopObjects>();
+            
+            // foreach (var buses in )
+            // {
+            //     
+            // }
+            //prints out the next 5 buses at our bus stop
+            // Console.WriteLine(response[0].destinationName);
         }
     }
 }
